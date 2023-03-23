c6505f79fe8580d0cf12778b7cdb98e9ef627187
ThomasSarlin <thomas.sarlin@gmail.com>
2023-03-23 12:14:33 +0100

    accidently called upon class method instead of instance method

--- src/producer.rb
+++ src/producer.rb
@@ -2,17 +2,18 @@ require './src/services/bunny-service.rb'
 require './src/services/rpi-service.rb'
 
 rabbitQueue = 'raspi-sunshine-monitor'
+updateRate = 20 #How often would you like to recieve updates from the raspberry Pi (seconds)
 bunnyService = BunnyService.new(rabbitQueue)
 rpiService = RpiService.new
 
 if !bunnyService.isActive || !rpiService.isActive
-  puts 'One or more services as not not active'
+  puts 'One or more services are not active'
   exit
 else
   puts 'Initializing service loop'
   while true do
-    BunnyService.sendSunshineData(rpiService.getSunshineData)
-    sleep(20)
+    bunnyService.sendSunshineData(rpiService.getSunshineData)
+    sleep(updateRate)
   end
   connection.close
 end

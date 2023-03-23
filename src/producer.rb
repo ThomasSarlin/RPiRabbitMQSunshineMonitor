afcc153d4d8b37b18c261c99a7d747076522e6c5
ThomasSarlin <thomas.sarlin@gmail.com>
2023-03-23 13:20:30 +0100

    added an easier way to alter the input port for rPi sensors

--- src/producer.rb
+++ src/producer.rb
@@ -5,10 +5,12 @@ rabbitQueueName = 'raspi-sunshine-monitor'
 
 #How often would you like to recieve updates from the raspberry Pi (seconds)
 updateRate = 20
+lightSensorInputPin = 4;
+temperatureInputPin = nil;
 
 #Initializing services.
 bunnyService = BunnyService.new(rabbitQueueName)
-rpiService = RpiService.new(4, nil)
+rpiService = RpiService.new(lightSensorInputPin, temperatureInputPin)
 
 if !bunnyService.isActive || !rpiService.isActive
   puts 'One or more services are not active'

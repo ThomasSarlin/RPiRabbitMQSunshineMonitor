7500a74bcc6544e91a967022e5f8ea1ed22fd51b
ThomasSarlin <thomas.sarlin@gmail.com>
2023-03-24 15:04:23 +0100

    fixed isActive

--- src/services/bunny-service.rb
+++ src/services/bunny-service.rb
@@ -16,7 +16,7 @@ class BunnyService
     end
   end
   def isActive
-    @connection.nil?
+    !@connection.nil?
   end
   def sendSunshineData(data)
     @channel.default_exchange.publish(data, routing_key: @queue.name)
